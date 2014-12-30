dfbf5fe731b9a49ad310b84b379f1af3c1eda5b1
Vishal <vishal@beenabled.com>
2014-12-30 17:36:30 +0530

    IOC - Added dummy cmd & tested. Will be taken out

---
+++ src/GenR2.DAL/Command/TestCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenR2.DAL.Command
+{
+    public class TestCommand
+    {
+        public string Message { get; set; }
+        public TestCommand()
+        {
+            this.Message = "Hello";
+        }
+    }
+}
